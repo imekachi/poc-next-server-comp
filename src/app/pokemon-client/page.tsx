93e654de1eb0b9a7d6ff5e1fc31c5a3fba6b44dd
imekachi <imekachi@gmail.com>
2023-03-27 15:18:53 -0700

    fix: properly wrap env and header in client-only fetching page

--- src/app/pokemon-client/page.tsx
+++ src/app/pokemon-client/page.tsx
@@ -1,5 +1,17 @@
+import { EnvLegend } from '@/modules/EnvLegend'
+import { PageHeader } from '@/modules/PageHeader'
 import { PokemonListWithQuery } from '@/modules/PokemonListWithQuery'
 
 export default function PokemonClientOnly() {
-  return <PokemonListWithQuery />
+  return (
+    <EnvLegend env="server">
+      <div className="p-6">
+        <PageHeader
+          title="Pokemon page"
+          description="Client-only data fetching"
+        />
+        <PokemonListWithQuery />
+      </div>
+    </EnvLegend>
+  )
 }
